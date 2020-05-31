1d15c67313cba16c71c8e2dbccf4b1eabb434047
alex-renyov <alex.renyov@gmail.com>
2020-05-31 18:09:42 +0300

    Added redux

---
+++ AppComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function AppComponent() {
+  return (
+    <div>It's app component</div>
+  );
+}
--- index.tsx
+++ index.tsx
@@ -1,31 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
+import { Provider } from 'react-redux';
+
 import './style.css';
 
-interface AppProps { }
-interface AppState {
-  name: string;
-}
+import { store } from './store';
+import App from './AppComponent';
 
-class App extends Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+render((
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  ),
+  document.getElementById('root')
+);
