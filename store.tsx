4f0fdeec7a473318a53396d8067a4b4e53d8fd40
alex-renyov <alex.renyov@gmail.com>
2020-05-31 20:56:17 +0300

    More fixes

--- store.tsx
+++ store.tsx
@@ -1,10 +1,10 @@
 import { configureStore, ThunkAction, Action, PayloadAction, createReducer, createSlice, createAsyncThunk, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { routerMiddleware, connectRouter } from 'connected-react-router';
 
-import { simpleReducer, simpleIncrement, simpleDecrement } from './store/01-simple';
-import { simple2Slice, simple2Increment, simple2Decrement } from './store/02-simple';
-import { simpleAsyncReducer, asyncLoadAction } from './store/03-async';
-import { asyncLoadAction2, asyncSlice2 } from './store/04-async';
+import { simpleReducer, simpleIncrement, simpleDecrement } from './store/04-simple';
+import { simple2Slice, simple2Increment, simple2Decrement } from './store/05-simple';
+import { simpleAsyncReducer, asyncLoadAction } from './store/06-async';
+import { asyncLoadAction2, asyncSlice2 } from './store/07-async';
 
 
 export function createStore(history: any) {
